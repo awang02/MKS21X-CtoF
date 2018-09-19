a1dedd9196e815ca429d5aec1fcc3c2dc0d216ba
Allen Wang <awang02@stuy.edu>
2018-09-19 15:16:10 -0400

    added test cases

--- CtoFTester.java
+++ CtoFTester.java
@@ -5,4 +5,13 @@ public class CtoFTester{
   public static int fahrenheitToCelsius(temp) {
     return ((temp - 32) * 5) / 9
   }
+
+  public static void main(String[] args){
+    System.out.println( celsiusToFahrenheit(0));
+    System.out.println( celsiusToFahrenheit(37));
+    System.out.println( celsiusToFahrenheit(-40));
+    System.out.println( fahrenheitToCelsius(212));
+    System.out.println( fahrenheitToCelsius(50));
+    System.out.println( fahrenheitToCelsius(-40));
+  }
 }
