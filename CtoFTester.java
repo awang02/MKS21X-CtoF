2fcbc2177378426b134e9942b477cd6007bd4c11
Allen Wang <awang02@stuy.edu>
2018-09-19 15:03:46 -0400

    added temperature functions

---
+++ CtoFTester.java
@@ -0,0 +1,8 @@
+public class CtoFTester{
+  public static int celsiusToFahrenheit(temp) {
+    return (1.8 * temp) + 32
+  }
+  public static int fahrenheitToCelsius(temp) {
+    return ((temp - 32) * 5) / 9
+  }
+}
